14d77c2383e2bf8b467cac8f0f13c0bd66bdf299
doduyquy <doduyquy211@gmail.com>
2026-05-15 17:41:02 +0700

    Init portfolio

---
+++ src/content.config.ts
@@ -0,0 +1,46 @@
+import { defineCollection, z } from "astro:content";
+
+// create collection: posts
+// a post need these fields
+const posts = defineCollection({
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        date: z.date(),
+        tags: z.array(z.string()).default([]),
+        lang: z.enum(["vi", "en"]).default("vi"),
+        draft: z.boolean().default(false),
+    }),
+});
+
+const projects = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    tech: z.array(z.string()).default([]),
+    github: z.string().optional(),
+    demo: z.string().optional(),
+    image: z.string().optional(),
+    featured: z.boolean().default(false), // good projects to show at home page
+  }),
+});
+
+const papers = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    venue: z.string().optional(),
+    year: z.number().optional(),
+    authors: z.array(z.string()).default([]),
+    abstract: z.string().optional(),
+    pdf: z.string(),
+    code: z.string().optional(),
+    status: z.enum(["approved", "processing"]).default("processing"),
+    featured: z.boolean().default(false), // good papers to show at home page
+  }),
+});
+
+export const collection = {
+    posts,
+    projects,
+    papers,
+};
---
+++ src/content/posts/diffusion_model.md
@@ -0,0 +1,20 @@
+---
+title: "What are Diffusion Models?"
+description: "Giải thích Diffusion Models từ trực giác đến công thức và triển khai."
+date: 2026-05-15
+tags: ["Diffusion", "Deep Learning", "Computer Vision"]
+lang: "vi"
+draft: false
+---
+
+So far, I’ve written about three types of generative models: GAN, VAE, and Flow-based models...
+
+## Forward diffusion process
+
+Diffusion models gradually add Gaussian noise to data...
+
+![Forward diffusion](/public/avatar_nii.png)
+
+## Reverse process
+
+The model learns how to denoise step by step...
